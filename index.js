ad5773d4cd538d2b352f5ef2692ec4491aa464f3
Prishal <prishalmakwana@gmail.com>
2022-05-08 23:22:05 +0100

    all education and experience sm button links

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Skills Button
 let skillDesktopButton = document.getElementById("skill-deskop-btn");
 let skillMobileButton = document.getElementById("skill-mobile-btn");
 skillDesktopButton.addEventListener("click", skillsButton);
@@ -33,12 +34,163 @@ function skillsButton() {
   }
 }
 
-function smJobDetail1() {
-  document.getElementById("job-detail-1").classList.toggle("hide");
-  document.getElementById("sm-job-detail-1").innerHTML = "View more";
+// Job detail 1 - futureproof
+
+function smJobDetailViewLessFutureproof() {
+  document.getElementById("view-futureproof").classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-less-futureproof")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-more-futureproof")
+    .classList.toggle("hide");
+}
+function smJobDetailViewMoreFutureproof() {
+  document.getElementById("view-futureproof").classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-less-futureproof")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-more-futureproof")
+    .classList.toggle("hide");
 }
 
-function smJobDetail2() {
-  document.getElementById("job-detail-2").classList.toggle("hide");
-  document.getElementById("sm-job-detail-2").innerHTML = "View more";
+// Job detail 2 - ichoosr
+function smJobDetailViewLessiChoosr() {
+  document.getElementById("view-ichoosr-1").classList.toggle("hide");
+  document.getElementById("view-ichoosr-2").classList.toggle("hide");
+  document.getElementById("view-ichoosr-3").classList.toggle("hide");
+  document.getElementById("view-ichoosr-4").classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-less-ichoosr")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-more-ichoosr")
+    .classList.toggle("hide");
+}
+
+function smJobDetailViewMoreiChoosr() {
+  document.getElementById("view-ichoosr-1").classList.toggle("hide");
+  document.getElementById("view-ichoosr-2").classList.toggle("hide");
+  document.getElementById("view-ichoosr-3").classList.toggle("hide");
+  document.getElementById("view-ichoosr-4").classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-less-ichoosr")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-job-detail-view-more-ichoosr")
+    .classList.toggle("hide");
+}
+
+// School detail 1 - UH
+function smSchoolDetailViewLessUH() {
+  document.getElementById("view-uh-1").classList.toggle("hide");
+  document.getElementById("view-uh-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-uh")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-uh")
+    .classList.toggle("hide");
+}
+
+function smSchoolDetailViewMoreUH() {
+  document.getElementById("view-uh-1").classList.toggle("hide");
+  document.getElementById("view-uh-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-uh")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-uh")
+    .classList.toggle("hide");
+}
+
+// School detail 2 - SU
+function smSchoolDetailViewLessSU() {
+  document.getElementById("view-su-1").classList.toggle("hide");
+  document.getElementById("view-su-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-su")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-su")
+    .classList.toggle("hide");
+}
+
+function smSchoolDetailViewMoreSU() {
+  document.getElementById("view-su-1").classList.toggle("hide");
+  document.getElementById("view-su-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-su")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-su")
+    .classList.toggle("hide");
+}
+// School detail 3 - WSU
+function smSchoolDetailViewLessWSU() {
+  document.getElementById("view-wsu-1").classList.toggle("hide");
+  document.getElementById("view-wsu-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-wsu")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-wsu")
+    .classList.toggle("hide");
+}
+
+function smSchoolDetailViewMoreWSU() {
+  document.getElementById("view-wsu-1").classList.toggle("hide");
+  document.getElementById("view-wsu-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-wsu")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-wsu")
+    .classList.toggle("hide");
+}
+// School detail 4 - ESSCA
+function smSchoolDetailViewLessESSCA() {
+  document.getElementById("view-essca-1").classList.toggle("hide");
+  document.getElementById("view-essca-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-essca")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-essca")
+    .classList.toggle("hide");
+}
+
+function smSchoolDetailViewMoreESSCA() {
+  document.getElementById("view-essca-1").classList.toggle("hide");
+  document.getElementById("view-essca-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-essca")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-essca")
+    .classList.toggle("hide");
+}
+
+// School detail 5 - CANDI
+function smSchoolDetailViewLessCANDI() {
+  document.getElementById("view-candi-1").classList.toggle("hide");
+  document.getElementById("view-candi-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-candi")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-candi")
+    .classList.toggle("hide");
+}
+
+function smSchoolDetailViewMoreCANDI() {
+  document.getElementById("view-candi-1").classList.toggle("hide");
+  document.getElementById("view-candi-2").classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-less-candi")
+    .classList.toggle("hide");
+  document
+    .getElementById("sm-school-detail-view-more-candi")
+    .classList.toggle("hide");
 }
