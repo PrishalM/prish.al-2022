85bde6e9900e319a65b80570942e2bf8f021ac22
Prishal <prishalmakwana@gmail.com>
2022-05-22 14:16:11 +0100

    contact form test 2

--- contact.php
+++ contact.php
@@ -1,54 +1,26 @@
 <?php
 
+$to = 'contact@prish.al';
 
-$errors = '';
-$myemail = 'contact@prish.al';
+$subject = "Prish.al Contact form from : $name";
 
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-
-
-
-
-if( empty($errors))
-
-{
-
-$to = $myemail;
-
-$email_subject = "Contact form submission: $name";
-
-$email_body = "You have received a new message. ".
+$message = "You have received a new message. ".
 
 " Here are the details:\n Name: $name \n ".
 
-"Email: $email_address\n Message \n $message";
+"Email: $email \n Message \n $message";
 
-$headers = "From: $myemail\n";
+$headers = array(
+    'From' => "From: $email\n",
+    'Reply-To' => 'To: contact@prish.al',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
 
-$headers .= "Reply-To: $email_address";
 
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$subject,$message,$headers);
 
-//redirect to the 'thank you' page
 
 header('Location: message-sent.html');
-}
+
 ?>
 
